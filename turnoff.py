4b4f74403db02229171fb387658eeb7b76975cff
Rafael Mastrocinque <andaime540@gmail.com>
2026-03-04 11:21:40 -0300

    teste

---
+++ turnoff.py
@@ -0,0 +1,10 @@
+import pyautogui
+import time
+pyautogui.hotkey('win', 'x')
+pyautogui.press('up')
+pyautogui.press('up')
+pyautogui.press('enter')
+pyautogui.press('up')
+pyautogui.press('up')
+time.sleep(1)
+pyautogui.press('enter')
